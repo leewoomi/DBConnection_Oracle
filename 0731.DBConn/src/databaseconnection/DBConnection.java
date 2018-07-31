3bfbe27d90dbda29e61b95f2bc1d24e670decaa9
leewoomi <woomi1114@gmail.com>
2018-07-31 18:22:25 +0900

    DBConnection_Oracle

---
+++ 0731.DBConn/src/databaseconnection/DBConnection.java
@@ -0,0 +1,36 @@
+package databaseconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	public static void main(String[] args) throws SQLException {
+		// 사용하고자 하는 드라이버 클래스를 로드
+		// 데이터베이스 종류에 따라 다름
+
+		// 데이터베이스 연결 변수 선언
+		// 반드시 close()를 호출해야 하므로 finally에서 close()를 호출
+		Connection con = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			System.out.println(con);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			// 연결이 되어 있으면 반드시 연결 해제
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+	}
+
+}
